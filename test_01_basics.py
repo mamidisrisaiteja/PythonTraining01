a17b8b01f7aa047eae1e97327c81ea95c46a1b00
saiteja <mamidisrisaiteja@gmail.com>
2025-07-15 08:28:18 +0530

    second commit

---
+++ test_01_basics.py
@@ -0,0 +1,5 @@
+print("Hello, World!")
+a=5
+print(a)
+print(f"The value of a is: {a}")
+print(type(a))
---
+++ test_02_datatypes.py
@@ -0,0 +1,12 @@
+my_list = [1, "Playwright", True, 5.14]
+print(my_list)
+print(my_list[1])
+print(len(my_list))
+print(my_list[-1])
+print(my_list[1:3])
+my_list.insert(2,"Training")
+print(my_list)
+my_list.append("Doing good")
+print(my_list)
+del my_list[0]
+print(my_list)
