fb0d5b2230eae4164403824e3b6d6e15cef65ad1
Anoosha-1992 <anoosha.kundurthi@gmail.com>
2025-07-16 11:27:47 +0530

    lists completed

---
+++ HelloWorld.py
@@ -0,0 +1,5 @@
+print("Hello, World!")
+a="Hello Python"
+print(a)
+print(f"The text of a is: {a}")
+print(type(a))
---
+++ lists.py
@@ -0,0 +1,35 @@
+my_list = [1, "Playwright", True, 5.14, 'tarun', 'anu', 'uzvis', 'jai']
+print(my_list)
+
+print(type(my_list))
+
+print(my_list[3])
+
+print(len(my_list))
+
+print(type(my_list[3]))
+
+print(my_list[1:3])    #
+
+print(my_list[0:8:3])   # split functionality with skipper
+
+print(my_list.append('alekhya'))
+
+print(my_list)
+
+print(my_list.insert(6,'Siva'))
+print(my_list)
+
+print(len(my_list))
+
+my_list.pop()
+print(my_list)
+
+my_list.pop(0)
+print(my_list)
+
+my_list.remove('Playwright')
+print(my_list)
+
+del my_list[1]
+print(my_list)
